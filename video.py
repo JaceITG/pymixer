37e7dcf17f45ed23d85787e02edc961d02332ffa
Scheisse <jjwazoo@gmail.com>
2022-12-15 01:04:36 -0500

    data folder

--- video.py
+++ video.py
@@ -17,7 +17,7 @@ def render(bg, audio_clips):
     audio_track = aucomp.CompositeAudioClip(clips)
     video = video.set_audio(audio_track)
     video = video.set_duration(end)
-    video.write_videofile('generated.mp4', fps=1, codec='libx264')
+    video.write_videofile('data/generated.mp4', fps=1, codec='libx264')
 
 if __name__ == "__main__":
-    render('thumb.png', ['alone.mp3', 'beautiful.mp3'])
+    render('data/thumb.png', ['data/alone.mp3', 'data/beautiful.mp3'])
